9de93f9105297dc053618d28b55eea58d62652c3
CrownLis <arkaim.ganibal@mail.ru>
2023-01-23 04:28:39 +0600

    feat: use svg icon how component

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,15 @@
-const {getDefaultConfig} = require('metro-config');
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: {sourceExts},
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
     transformer: {
-      babelTransformerPath: require.resolve('react-native-sass-transformer'),
+      babelTransformerPath: require.resolve('./transformer.js'),
     },
     resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
       sourceExts: [...sourceExts, 'scss', 'sass', 'svg'],
     },
   };
